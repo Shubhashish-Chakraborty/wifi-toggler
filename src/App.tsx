6df20968c62c96b2a309f8bb9d5748788806a092
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-01-25 19:14:04 +0530

    started with frontend

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+export default function App() {
+    return (
+        <>
+            <div className="bg-red-700 text-white text-center transition-all duration-500 text-2xl md:text-4xl font-bold p-5">
+                <h1 className="animate-bounce">
+                    Coming Soon!!
+                </h1>
+            </div>
+        </>
+    )
+}
